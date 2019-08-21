5a2574447d67e341d66389b38af093b1b6cbf7f3
valentin202 <kibirevvalik202@gmail.com>
2019-08-21 11:58:41 +0300

    project Start

---
+++ 3 in a row/Assets/Scripts/NotationTransform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotationTransform : MonoBehaviour {
+
+    private Movement Getmove;
+    
+    private GameObject Manager;
+
+    private void Start()
+    {
+        Manager = GameObject.FindWithTag("Manager");
+        Getmove = Manager.GetComponent<Movement>();
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("0");
+              if (Getmove.obj1==null)
+              {                
+              Getmove.obj1 = gameObject;
+            Debug.Log("1");
+                }
+              else
+            {              
+            Getmove.obj2 = gameObject;
+          Debug.Log("2");
+         }
+
+
+    }
+   
+}
---
+++ 3 in a row/Assets/Scripts/Spawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject elementPink, elementGren, elementBloo;
+
+    private int spawnCount;
+    private void Start()
+    {
+        for (int x = -9; x <= 9; x += 2)
+        {
+            for (int y = -9; y <= 9; y += 2)
+            {
+                int rnd = Random.Range(0, 6);
+
+                if(rnd==0||rnd==2)
+                Instantiate(elementPink, new Vector2(x, y), Quaternion.identity);
+
+                if(rnd==1||rnd==4)
+                    Instantiate(elementGren, new Vector2(x, y), Quaternion.identity);
+
+                if(rnd==3||rnd==5)
+                    Instantiate(elementBloo, new Vector2(x, y), Quaternion.identity);
+
+            }
+        }
+    }
+}
